a55e190b3e8b8f15e041495b1d2c12b8ab865af9
SHUcream00 <requiemdeciel@gmail.com>
2020-03-14 19:04:21 +0900

    Mergesort incomplete

---
+++ MergeSort.py
@@ -0,0 +1,42 @@
+'''
+Basic idea to divide and conquer
+goal: nlogn
+
+'''
+class Array():
+    #to be sorted
+    def __init__(self, array = []):
+        self.array = array
+
+    def mergesort(self, array):
+
+        if len(array) == 1: return array
+
+        mid = len(array] // 2
+        left = array[0:mid]
+        right = array[mid:]
+
+        self.mergesort(left)
+        self.mergesort(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                array[k] = left[i]
+                i += 1
+
+            else:
+                array[k] = right[j]
+                j += 1
+
+            k += 1
+
+        while i < len(left):
+            array[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            array[k] = right[j]
+            j += 1
+            k += 1
